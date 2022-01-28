bba2535bc9af2bcee61b1627690834ba643fb6d9
Michael <mikophg2001@gmail.com>
2022-01-27 19:48:51 -0800

    redraw_window and format_time for the GUI

--- GUI.py
+++ GUI.py
@@ -134,5 +134,26 @@ class Cube:
     def set_temp(self, val):
         self.temp = val
 
+def redraw_window(win, board, time, strikes):
+    win.fill((255,255,255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw(win)
+
+
+def format_time(secs):
+    sec = secs%60
+    minute = secs//60
+    hour = minute//60
+
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
+
 
 
